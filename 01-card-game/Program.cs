ac643be92050df210665b7368ab88f30b0174ebf
popados <popados@gmail.com>
2022-04-22 16:07:13 -0500

    added original program

---
+++ 01-card-game/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _00_card_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
